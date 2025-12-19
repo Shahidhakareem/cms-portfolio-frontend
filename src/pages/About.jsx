f65f3bc5a44f6dbde93ba2e78153e33004c56945
Shahidhakareem <shahidhakareem134@gmail.com>
2025-12-19 22:58:34 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,30 +3,20 @@ import api from "../api/axios";
 
 export function About() {
   const [about, setAbout] = useState(null);
-  const [loading, setLoading] = useState(true);
 
- useEffect(() => {
-  fetch("https://cms-portfolio-backend.onrender.com/api/about")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("FETCH DATA:", data);
-      setAbout(data); // ✅ REQUIRED
-      setLoading(false);
-    })
-    .catch(console.error);
-}, []);
+  useEffect(() => {
+    api.get("/about").then((res) => setAbout(res.data));
+    
+  }, []);
 
-  if (loading) return <p>Loading...</p>;
-  if (!about) return <p>No data found</p>;
+  if (!about) return null;
 
   return (
     <div>
+      {/* ================= HERO ================= */}
       <section className="bg-gray-900 text-white py-20 px-6">
-        <div className="max-w-4xl mx-auto text-center">
-          <h1 className="text-5xl font-bold mb-6 font-serif italic">
-            About Me
-          </h1>
-
+        <div className="max-w-4xl mx-auto text-center animate-fade-in-up">
+          <h1 className="text-5xl font-bold mb-6 font-serif italic ">About Me</h1>
           <p className="text-lg text-gray-300 leading-relaxed font-serif italic">
             {about.bioText}
           </p>
